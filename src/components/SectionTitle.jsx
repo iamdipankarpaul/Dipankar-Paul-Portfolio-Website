99bedeae41e17fc607d1ddff6d8f78449db9992d
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-06 23:52:19 +0530

    added under line on hover

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,12 +1,18 @@
 import { Title, Divider, Box } from "@mantine/core";
+import { useHover } from "@mantine/hooks";
 
 const SectionTitle = ({ text }) => {
+  const { hovered, ref: sectionTitleRef } = useHover();
   return (
     <Box mb="sm">
       <Title
+        ref={sectionTitleRef}
         fw={500}
         fz={{ base: "h2", sm: "h1" }}
         my={{ base: "md", sm: "lg" }}
+        style={{ textDecoration: hovered ? "underline" : "none",
+          textUnderlinePosition: "under"
+         }}
       >
         {text}
       </Title>
