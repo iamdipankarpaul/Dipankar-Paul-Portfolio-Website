5aa15fdc53a9919703572b9473333945fddaf522
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-10 12:53:29 +0530

    working on contact page

---
+++ src/layout/AppFooter.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mantine/core";
+import classes from "./AppLayout.module.css";
+
+const AppFooter = () => {
+  return (
+    <footer>
+      <Box>AppFooter</Box>
+    </footer>
+  );
+};
+
+export default AppFooter;
