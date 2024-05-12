812b85a91fd87ab84c061116d487857028850394
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-12 14:40:30 +0530

    getting started with the blogs page

--- src/pages/blogs/BlogsPage.jsx
+++ src/pages/blogs/BlogsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Box } from "@mantine/core";
-import { useScrollIntoView } from "@mantine/hooks";
+import { useScrollIntoView, useFetch } from "@mantine/hooks";
 import { Article } from "@phosphor-icons/react";
 
 import classes from "./Blogs.module.css";
@@ -17,11 +17,42 @@ const BlogsPage = () => {
     scrollIntoView({ alignment: "start" });
   }, []);
 
+  const { data, loading, error, refetch, abort } = useFetch(
+    "https://dev.to/api/articles?username=dipankarpaul"
+  );
+
+  let mappedData = null;
+  if (data) {
+    mappedData = data.map(
+      ({
+        description,
+        id,
+        published_at,
+        readable_publish_date,
+        reading_time_minutes,
+        tag_list,
+        tags,
+        title,
+        url,
+      }) => ({
+        description,
+        id,
+        published_at,
+        readable_publish_date,
+        reading_time_minutes,
+        tag_list,
+        tags,
+        title,
+        url,
+      })
+    );
+  }
+
   return (
-    <Box className={classes.wrapper}>
+    <Box className={classes.wrapper} ref={wrapperRef}>
       <Topbar label="Blogs" icon={<Article size={24} />} />
-      <Box>
-        <div>Blogs Page</div>
+      <Box py={{ base: "sm", sm: "lg" }} px={{ base: "xs", sm: "lg" }}>
+        <Box>{!mappedData ? "loading" : "Blogs Page"}</Box>
       </Box>
     </Box>
   );
